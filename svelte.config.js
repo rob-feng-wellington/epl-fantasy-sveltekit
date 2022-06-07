d3d902de22bb977dbf3333d57e6a6c8b8531ccfa
Rob Feng <rob.feng.wellington@gmail.com>
2022-06-07 09:07:18 +0000

    replace material with carbon and installed carbon diagram

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+/* import preprocess from 'svelte-preprocess'; */
+import { optimizeImports } from "carbon-preprocess-svelte";
 
+const production = process.env.NODE_ENV === 'production';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [optimizeImports()],
 
 	kit: {
 		adapter: adapter(),
@@ -16,7 +18,13 @@ const config = {
 					clientPort: process.env.HMR_HOST ? 443 : 3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : 'localhost'
 				}
-			}
+			},
+			optimizeDeps: {
+				include: ['@carbon/charts'],
+			},
+			ssr: {
+				noExternal: [production && '@carbon/charts'].filter(Boolean),
+			},
 		}
 	}
 };
